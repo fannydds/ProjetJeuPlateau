a0d6c628e19af27b188bf416f5981b337052a4ac
fannydds <fanny.daudies@etu.univ-lyon1.fr>
2017-04-11 15:59:10 +0200

    Classe Grille controller

---
+++ JeuPlateau/src/lib/jeuplateau/modele/GrilleJeuController.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib.jeuplateau.modele;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.ColumnConstraints;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.RectangleBuilder;
+
+/**
+ *
+ * @author Epulapp
+ */
+public class GrilleJeuController implements Initializable {
+    
+    @FXML
+    private GridPane grilleGridPane;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initGrilleGridPane();
+    }   
+    
+    private void initGrilleGridPane(){
+        final int numCols = 20 ;
+        final int numRows = 20;
+        grilleGridPane.getRowConstraints().clear();
+        grilleGridPane.getColumnConstraints().clear();
+        for (int i = 0; i < numCols; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / numCols);
+            grilleGridPane.getColumnConstraints().add(colConst);
+//            Rectangle r = new Rectangle();
+//            r.autosize();
+//            r.setFill(Color.CHARTREUSE);
+//            grilleGridPane.add(r, i, 0);
+        }
+        for (int i = 0; i < numRows; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / numRows);
+            grilleGridPane.getRowConstraints().add(rowConst);         
+        }
+        grilleGridPane.setGridLinesVisible(true);
+
+        System.out.println(grilleGridPane.getColumnConstraints());
+        for (int i = 0; i < numCols; i++) {
+            for (int j = 0; j < numRows; j++) {
+                Label r = new Label();
+                r.setStyle("-fx-background-color: black;");
+                AnchorPane ap = new AnchorPane();
+                
+                ap.setTopAnchor(r, 0.5);
+                ap.setLeftAnchor(r, 0.5);
+                ap.setRightAnchor(r, 0.5);
+                ap.setBottomAnchor(r, 0.5);
+                ap.getChildren().add(r);
+                grilleGridPane.add(ap, i, j);
+            }
+        }
+    }
+    
+}
